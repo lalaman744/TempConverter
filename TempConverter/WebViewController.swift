ff126638dd93ad4d8d78b04ca89df41ba3ac393b
Josh Kleinschmidt <lalaman744@hotmail.com>
2019-02-19 17:03:21 -0600

    Attempted to add web page tab

---
+++ TempConverter/WebViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mctcHomePage = URL(string:"https://www.minneapolis.edu")
+        let myRequest = URLRequest(url: mctcHomePage!)
+        webView.load(URLRequest(url: mctcHomePage!))
+        webView.load(myRequest)
+    }
+}
